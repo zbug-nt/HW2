0e74e590947ad1ad5f87015232feb67338d41b0e
zbug-nt <2569169388@qq.com>
2019-03-07 10:49:46 +0800

    Hello1

---
+++ Hello1/Hello1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.printArgs(args);
+        }
+    }
+
+    class HelloWorld
+    {
+        public HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void printArgs(string[] args)
+        {
+            Console.WriteLine($"There are {args.Length} argument(s).\nThey are:");
+            for (int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
