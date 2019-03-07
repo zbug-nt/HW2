dd0344b40dc51a3959bb43a051cdae7ea4a345fa
zbug-nt <2569169388@qq.com>
2019-03-07 11:02:07 +0800

    Hello2

---
+++ Hello2/Hello2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.Hello();
+            HelloWorld.PrintArgs(args);
+        }
+    }
+
+    class HelloWorld
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void PrintArgs(string[] args)
+        {
+            Console.WriteLine($"There are {args.Length} argument(s).\nThey are:");
+            for (int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
