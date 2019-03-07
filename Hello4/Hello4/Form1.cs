cdcbbe570c0f8aed2981b5beac73467904d2c715
zbug-nt <2569169388@qq.com>
2019-03-07 19:17:57 +0800

    Meow~

--- Hello4/Hello4/Form1.cs
+++ Hello4/Hello4/Form1.cs
@@ -12,9 +12,29 @@ namespace Hello4
 {
     public partial class Form1 : Form
     {
+        private String[] args;
         public Form1()
         {
             InitializeComponent();
+            args = Environment.GetCommandLineArgs();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String msg = "Hello World!\n";
+            if (args.Length == 0)
+            {
+                msg += "There is no argument.\n";
+            }
+            else
+            {
+                msg += $"There are {args.Length} argument(s).\nThey are:\n";
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    msg += args[i] + "\n";
+                }
+            }
+            this.label1.Text = msg;
         }
     }
 }
